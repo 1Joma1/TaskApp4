1891ccbdcd3e7f1aa97a68f4bc1f15972ba8d1c2
Jamaldin <47467889+1Joma1@users.noreply.github.com>
2019-05-12 06:09:31 +0600

    Update PageViewModel.java

--- app/src/main/java/com/geektech/taskapp4/ui/main/PageViewModel.java
+++ app/src/main/java/com/geektech/taskapp4/ui/main/PageViewModel.java
@@ -8,8 +8,8 @@ import android.arch.lifecycle.ViewModel;
 
 public class PageViewModel extends ViewModel {
 
-    public private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
-    public private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
+    public MutableLiveData<Integer> mIndex = new MutableLiveData<>();
+    public LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
             return ""+input;
